ba8d4bd421a9378edfe41b919ca1a16baf4787c1
Aimer Neige <aimer@linux.com>
2026-04-17 12:56:24 +0000

    change link to github

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-aiur() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://gitlab.aiursoft.com/aiursoft/aiurscript/-/raw/master/$1.sh | sudo bash -s $arg; }
+aiur() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://github.com/aiursoftweb/aiurscript/raw/master/$1.sh | sudo bash -s $arg; }
 
 app_name="employeeCenter"
-repo_path="https://gitlab.aiursoft.com/aiursoft/employeeCenter"
+repo_path="https://github.com/aiursoftweb/employeeCenter"
 proj_path="src/Aiursoft.EmployeeCenter/Aiursoft.EmployeeCenter.csproj"
 
 get_dll_name()
