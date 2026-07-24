d8cdbc3e0d1db3e310ce4482add744af58524993
Dvorak Chen <dvorak@aiursoft.com>
2026-07-24 15:45:54 +0800

    todo: wait to valid

--- check_viewmodels.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file in $(find src/Aiursoft.EmployeeCenter/Models -name "*ViewModel.cs"); do
-    # Check if the class inherits from UiStackLayoutViewModel or similar, or is just a ViewModel
-    if ! grep -q "PageTitle" "$file"; then
-        echo "Missing PageTitle: $file"
-    fi
-done
